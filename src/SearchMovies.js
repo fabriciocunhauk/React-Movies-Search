d9937bfcb5f476235777ada4d5c2bd553c005884
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-05-30 04:38:14 +0100

    app start

---
+++ src/SearchMovies.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SearchMovies() {
+    return (
+        <form className="form">
+            <label className="label" htmlFor="query">Movie Name</label>
+            <input className="input" type="text" name="query" placeholder="i.e Juracic Park" />
+            <button className="button" type="submit">Search</button>
+        </form>
+    )
+}
